c36d81846a3222b10e9376b4100ad6c67598f1d6
Daniel Miller <dmiller@cynosure.com>
2015-04-20 16:27:17 -0400

    update 4-20-14

--- plugin.php
+++ plugin.php
@@ -48,13 +48,14 @@ class Cynosure_Media_Query
 	}
 
 	function generateMediaQueryArgs() {
-
+		
 		$args = array(
 
 				'post_status' => 'publish',
 				'post_type' => 'cynosure-material',
 				'tax_query' => $this->generateTaxQuery()
 			);
+
 
 
 		return $args;
@@ -72,7 +73,6 @@ class Cynosure_Media_Query
 
 
 }
-
 
 class Cynosure_Material_Data_Set
 {
@@ -100,7 +100,7 @@ class Cynosure_Material_Data_Set
 		$get_all = new Cynosure_Media_Query(array('criteria' => array('media-product' => $this->product)));
 
 		$get_all_query = $get_all->runQuery();
-
+		
 		if($get_all_query->have_posts()) {
 
 			while ($get_all_query->have_posts()) {
@@ -111,6 +111,7 @@ class Cynosure_Material_Data_Set
 
 			}
 
+			
 		}
 
 		wp_reset_postdata();
@@ -154,6 +155,8 @@ class Cynosure_Material_Data_Set
 
 		}
 
+		wp_reset_postdata();
+
 	}
 
 	function get_product_categories() {
@@ -171,6 +174,8 @@ class Cynosure_Material_Data_Set
 			}
 
 		}
+
+		wp_reset_postdata();
 
 	}
 
@@ -216,8 +221,125 @@ class Cynosure_Material_Data_Set
 			}
 		}
 
+		wp_reset_postdata();
+
 
 
 	}
+
+}
+
+class Cynosure_Paginator 
+{
+	function __construct($args) {
+
+		$this->items = $args['items'];
+		$this->items_count = count($args['items']);
+		$this->items_per_page = $args['items_per_page'];
+		$this->page = $args['page'];
+		$this->num_pages = ceil($this->items_count / $this->items_per_page);
+
+	}
+
+	function get_start_point() {
+
+		return $this->items_per_page * ($this->page-1);
+
+	}
+
+	function get_end_point() {
+
+		return $this->get_start_point() + $this->items_per_page-1;
+
+	}
+
+	function items_on_page() {
+
+
+		$start_point = $this->get_start_point();
+		$end_point = $this->get_end_point();
+		$items_on_page = array();
+
+
+
+		for($i = $start_point; $i <= $end_point; $i++) {
+			
+			if($i < $this->items_count) {
+
+				array_push($items_on_page, $this->items[$i]);
+
+			}
+
+		}
+
+
+		return $items_on_page;
+
+	}
+
+	function display_pagination() {
+
+		$pages = null;
+
+		if ($this->num_pages > 1) {
+
+			ob_start();
+			
+			
+			?>	
+				<div class = "pages-container">
+					
+					<?php if ($this->page != 1): ?>
+						<span class = "pagination first"><a href = "<?= $this->build_url(1) ?>">First</a></span>
+						<span class = "pagination previous"><a href = "<?= $this->build_url($this->page - 1) ?>">Previous</a></span>
+					<?php endif; ?>
+
+			
+
+			<?php for($i=1; $i <= $this->num_pages; $i++): ?>
+
+					<?php $class = $i == $this->page ? "pagination page-num active" : "pagination page-num"; ?>
+					<span class = "<?= $class ?>">
+						<a href = "<?= $this->build_url($i) ?>"><?= $i ?></a>
+					</span>
+
+				
+
+			<?php endfor; ?>
+
+			<?php if ($this->page != $this->num_pages): ?>
+
+				<span class = "pagination last"><a href = "<?= $this->build_url($this->num_pages) ?>">Last</a></span>
+				<span class = "pagination next"><a href = "<?= $this->build_url($this->page + 1) ?>">Next</a></span>
+
+			<?php endif; ?>
+
+			<?php 
+
+			$pages = ob_get_contents();
+
+			ob_end_clean();
+
+		}
+
+		return $pages;
+
+	}
+
+	private function build_url($page_num) {
+		$post = get_post(get_the_ID());
+		$slug = $post->post_name;
+		$base_url = get_site_url();
+
+		$query_string = $_SERVER['REDIRECT_QUERY_STRING'] ? '?' . $_SERVER['REDIRECT_QUERY_STRING'] : null;
+
+		$url = $base_url . '/products/' . $slug . '/' . $page_num . '/' . $query_string;
+
+		return $url;
+	}
+
+	
+
+
 
 }
